592526679d9e83d1c784b2c2faf5657b89783d2d
Mai Cong Danh 19021233 <62579415+wowyman@users.noreply.github.com>
2021-12-11 04:53:07 +0700

    giải thể folder backend, gửi list câu hỏi qua socket
    shiba

--- server.js
+++ server.js
@@ -2,14 +2,43 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+//return the guest page
 app.get('/', function(req, res) {
-    //res.sendFile(process.cwd() + "//index.html");
-    res.sendFile("C:\\Users\\pc\\OneDrive\\Tài liệu\\GitHub\\rank-up-challenge\\index.html");
+    res.sendFile(__dirname + "/test.html");
+    //res.sendFile("C:\\Users\\pc\\OneDrive\\Tài liệu\\GitHub\\rank-up-challenge\\index.html");
 });
+
+//return the host page
+// app.get('/host', function(req, res) {
+//     res.sendFile(__dirname + "/test.html");
+//     //res.sendFile("C:\\Users\\pc\\OneDrive\\Tài liệu\\GitHub\\rank-up-challenge\\index.html");
+// });
 
 users = [];
 var room_id = 1;
 var room_code = '123';
+
+let questions = [{
+        question: "How to tell if a programming language is turing complete?",
+        imgSrc: "https://d1ymz67w5raq8g.cloudfront.net/Pictures/480xany/6/5/5/509655_shutterstock_1506580442_769367.jpg",
+        A: "Ask the professor",
+        B: "Ask the professor",
+        C: "Ask the professor",
+        D: "Ask the professor",
+        correctAnswer: "A"
+    },
+    {
+        question: "Is CSS a programming language?",
+        imgSrc: "",
+        A: "Yes",
+        B: "Yes, it is",
+        C: "No, it isn't",
+        D: "No",
+        correctAnswer: "D"
+    },
+
+];
+
 io.on('connection', function(socket) {
     var roomCodeIsCorrect = false;
     socket.on('checkRoomCode', function(roomcode) {
@@ -37,6 +66,9 @@ io.on('connection', function(socket) {
 
     })
 
+    //----------Send list question---------------
+    socket.emit('ListQuestion', questions);
+    //-------------------------------------------
 
 
 });
