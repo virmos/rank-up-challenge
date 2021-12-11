23021e38f881674d7b9107bd7dd7a521aab867fe
Mai Cong Danh 19021233 <62579415+wowyman@users.noreply.github.com>
2021-12-11 14:42:11 +0700

    update multiplayer-quiz

--- server.js
+++ server.js
@@ -15,23 +15,22 @@ const __dirname = dirname(__filename);
 app.use(express.static(path.join(__dirname, './public/')))
 
 //return the guest login page
-app.get('/login', function(req, res) {
-    res.sendFile(__dirname + "/login.html");
+app.get('/guest', function(req, res) {
+    res.sendFile(__dirname + "/guest.html");
 });
 
 app.get('/game', function(req, res) {
-    res.sendFile(__dirname + "/test.html");
+    res.sendFile(__dirname + "/index.html");
 });
 
-////return the host page
-// app.get('/host', function(req, res) {
-//     res.sendFile(__dirname + "/test.html");
-// });
+//return the host page
+app.get('/host', function(req, res) {
+    res.sendFile(__dirname + "/host.html");
+});
 
 let users = [];
 let room_id = 1;
-let room_code = '123';
-
+let room_code;
 let questions = [{
         question: "How to tell if a programming language is turing complete?",
         imgSrc: "https://d1ymz67w5raq8g.cloudfront.net/Pictures/480xany/6/5/5/509655_shutterstock_1506580442_769367.jpg",
@@ -54,30 +53,33 @@ let questions = [{
 ];
 
 io.on('connection', function(socket) {
+    socket.on('create', function(data) {
+        room_code = data.room_code;
+        //hostname la users[0]
+        users.push(data.name);
+        socket.emit('created', "Created room");
+        socket.emit('joined', true);
+    });
 
     socket.on('setUsername', function(data) {
         if (data.room_code == room_code) {
             if (users.indexOf(data.name) > -1) {
-
                 socket.emit('userExists', data.name + ' username is taken! Try some other username.');
             } else {
-
                 users.push(data.name);
                 socket.emit('userSet', { username: data.name });
                 socket.join("room-" + room_id);
-
-                //----------Send list question---------------
-                socket.emit('ListQuestion', questions);
-                //-------------------------------------------
-
-                io.sockets.in("room-" + room_id).emit('connectToRoom', data.name + " has joined room no." + room_id);
+                socket.emit('joined', true);
+                io.sockets.in("room-" + room_id).emit('connectToRoom', data.name + " has joined room");
             }
         } else {
             socket.emit('WrongRoomCode', 'Room code is false ! Type again !');
         }
 
-    })
-
+    });
+    //----------Send list question---------------
+    socket.emit('ListQuestion', questions);
+    //-------------------------------------------
 
 
 
